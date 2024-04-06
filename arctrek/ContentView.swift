9c5b9ea532721f73860d2aced7bfe24cc9c0cb6c
sabrinalou3 <87052249+sabrinalou3@users.noreply.github.com>
2024-04-06 16:44:17 -0700

    Initial Commit

---
+++ arctrek/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  arctrek
+//
+//  Created by Sabrina Lou on 2024-04-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ arctrek/arctrekApp.swift
@@ -0,0 +1,17 @@
+//
+//  arctrekApp.swift
+//  arctrek
+//
+//  Created by Sabrina Lou on 2024-04-06.
+//
+
+import SwiftUI
+
+@main
+struct arctrekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
